23f8d5b9589d9e411dfcdef407fa4c77034a00fe
Jacques Schutte <jqschutte@gmail.com>
2022-12-01 07:13:17 +0200

    Day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,18 +1,46 @@
 use util::*;
 
-struct Day01;
+#[derive(Debug)]
+struct Elf {
+    food: Vec<usize>,
+}
+
+#[derive(Debug)]
+struct Day01 {
+    elves: Vec<Elf>,
+}
 
 impl Day for Day01 {
     fn parse_input(input: &str) -> Self {
-        todo!()
+        let elves = input
+            .split("\n\n")
+            .map(|e| Elf {
+                food: e.lines().map(|f| f.parse().expect("Not food")).collect(),
+            })
+            .collect();
+
+        Day01 { elves }
     }
 
     fn part1(&self) -> String {
-        todo!()
+        self.elves
+            .iter()
+            .map(|e| e.food.iter().sum::<usize>())
+            .max()
+            .expect("No elf")
+            .to_string()
     }
 
     fn part2(&self) -> String {
-        todo!()
+        let mut sorted: Vec<_> = self
+            .elves
+            .iter()
+            .map(|e| e.food.iter().sum::<usize>())
+            .collect();
+
+        sorted.sort();
+
+        sorted.iter().rev().take(3).sum::<usize>().to_string()
     }
 
     fn number() -> u8 {
@@ -23,4 +51,3 @@ impl Day for Day01 {
 fn main() {
     Day01::run();
 }
-
