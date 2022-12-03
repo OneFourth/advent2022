52166d8568d9c9c8e2a310436e2da10b22dc987b
Jacques Schutte <jqschutte@gmail.com>
2022-12-03 07:35:04 +0200

    Day 3

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,18 +1,94 @@
+use std::collections::HashSet;
+
 use util::*;
 
-struct Day03;
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+enum Item {
+    Lower(char),
+    Upper(char),
+}
+
+impl Item {
+    fn priority(&self) -> usize {
+        match self {
+            Item::Lower(l) => 1 + (*l as usize - 'a' as usize),
+            Item::Upper(u) => 27 + (*u as usize - 'A' as usize),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Rucksack {
+    items: Vec<Item>,
+}
+
+impl Rucksack {
+    fn item_type(&self) -> Item {
+        let (left, right) = self.items.split_at(self.items.len() / 2);
+        let left: HashSet<_> = left.iter().copied().collect();
+        let right: HashSet<_> = right.iter().copied().collect();
+
+        *left
+            .intersection(&right)
+            .next()
+            .expect("Could not find match")
+    }
+}
+
+#[derive(Debug)]
+struct Day03 {
+    bags: Vec<Rucksack>,
+}
 
 impl Day for Day03 {
     fn parse_input(input: &str) -> Self {
-        todo!()
+        let bags = input
+            .lines()
+            .map(|s| Rucksack {
+                items: s
+                    .chars()
+                    .map(|c| {
+                        if c.is_lowercase() {
+                            Item::Lower(c)
+                        } else {
+                            Item::Upper(c)
+                        }
+                    })
+                    .collect(),
+            })
+            .collect();
+
+        Self { bags }
     }
 
     fn part1(&self) -> String {
-        todo!()
+        self.bags
+            .iter()
+            .map(|b| b.item_type().priority())
+            .sum::<usize>()
+            .to_string()
     }
 
     fn part2(&self) -> String {
-        todo!()
+        self.bags
+            .chunks(3)
+            .map(|v| {
+                let [a, b, c] = v else { panic!("Invalid chunk") };
+
+                let a: HashSet<_> = a.items.iter().copied().collect();
+                let b: HashSet<_> = b.items.iter().copied().collect();
+                let c: HashSet<_> = c.items.iter().copied().collect();
+
+                a.intersection(&b)
+                    .copied()
+                    .collect::<HashSet<_>>()
+                    .intersection(&c)
+                    .next()
+                    .expect("Could not find item")
+                    .priority()
+            })
+            .sum::<usize>()
+            .to_string()
     }
 
     fn number() -> u8 {
@@ -23,4 +99,3 @@ impl Day for Day03 {
 fn main() {
     Day03::run();
 }
-
