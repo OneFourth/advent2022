e00a8f4ad9ae396d80dc3e0c1a8a0dc68a84c3d4
Jacques Schutte <jqschutte@gmail.com>
2022-12-06 07:15:07 +0200

    Day 6

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,18 +1,34 @@
 use util::*;
 
-struct Day06;
+struct Day06 {
+    data: Vec<char>,
+}
+
+impl Day06 {
+    fn find_unique_window(&self, size: usize) -> usize {
+        self.data
+            .windows(size)
+            .enumerate()
+            .find(|&(_, v)| (0..size).all(|i| v[(i + 1)..].iter().all(|&c| v[i] != c)))
+            .unwrap()
+            .0
+            + size
+    }
+}
 
 impl Day for Day06 {
     fn parse_input(input: &str) -> Self {
-        todo!()
+        Self {
+            data: input.chars().collect(),
+        }
     }
 
     fn part1(&self) -> String {
-        todo!()
+        self.find_unique_window(4).to_string()
     }
 
     fn part2(&self) -> String {
-        todo!()
+        self.find_unique_window(14).to_string()
     }
 
     fn number() -> u8 {
@@ -23,4 +39,3 @@ impl Day for Day06 {
 fn main() {
     Day06::run();
 }
-
